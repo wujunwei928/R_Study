aba61b9a8d4dc77d5bfa5d413a6d496bdc08a7f0
wujunwei <1399952803@qq.com>
2015-12-30 16:18:03 +0800

    R文件命令行安装R包的时候指定R包镜像, 否则会报错

--- set_R_Environment.R
+++ set_R_Environment.R
@@ -1,14 +1,17 @@
 
 # 批量安装R包, 在set_R_Environment.sh 中 安装好之后在需要的软件之后, 调用此文件安装
 
+# 指定下载R package的镜像, 选一个速度快的
+downloadRepos <- 'http://mirror.bjtu.edu.cn/cran/'
+
 # 安装R包
-install.packages('rJava') 
+install.packages('rJava', repos = downloadRepos) 
 # 批量安装R包
-install.packages(c('RMongo', 'rredis', 'XML', 'xml2', 'rPython'))
+install.packages(c('RMongo', 'rredis', 'XML', 'xml2', 'rPython'), repos = downloadRepos)
 # 安装R语言的七种武器
-install.packages(c('RCurl', 'shiny', 'ggplot2', 'plyr', 'tm', 'htmlwidgets', 'quantmod'))
+install.packages(c('RCurl', 'shiny', 'ggplot2', 'plyr', 'tm', 'htmlwidgets', 'quantmod'), repos = downloadRepos)
 # 从github安装R包
-install.packages("devtools")
+install.packages("devtools", repos = downloadRepos)
 devtools::install_github("twitter/AnomalyDetection")	#机器学习包
 devtools::install_github('yihui/runr')	# 在R中调用BASH
 devtools::install_github("hadley/httr")	# 对RCurl进行了友好的封装
