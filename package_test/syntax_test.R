dfe40e3522c0b08a49d02c421d2992181596fbed
wujunwei <1399952803@qq.com>
2016-01-04 00:16:30 +0800

    datetime test

---
+++ package_test/syntax_test.R
@@ -0,0 +1,24 @@
+
+# R help
+help("RMySQL")
+?"RMySQL-package"
+help.search('rredis')
+apropos('topic')
+demo()
+?example
+
+# datetime test
+?Date
+Sys.Date()  # [1] "2016-01-04"
+Sys.time()  #[1] "2016-01-04 00:03:55 CST"
+format(Sys.time(), "%Y-%m-%d %H:%M:%S") # [1] "2016-01-04 00:06:01"
+
+
+
+
+
+
+
+
+
+
