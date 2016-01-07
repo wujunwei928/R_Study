f0dbd5072110f1b330e8b6dd91df752aa51f116a
wujunwei <1399952803@qq.com>
2016-01-07 13:51:49 +0800

    update setup file

--- set_R_Environment.sh
+++ set_R_Environment.sh
@@ -6,13 +6,16 @@ sudo ln -s  /usr/lib/jvm/java-7-openjdk-amd64/  /usr/lib/jvm/default-java	# reso
 sudo apt-get install libcurl4-openssl-dev	# resolve install RCurl package
 sudo apt-get install libxml++2.6-dev	# resolve install XML package
 sudo apt-get install libmysqlclient-dev		# resolve install RMySQL package
-
+sudo apt-get install libssl-dev 			# resolve install devtools package
 
 # install latest R
 sudo apt-add-repository -y "deb http://mirror.bjtu.edu.cn/cran/bin/linux/ubuntu/ `lsb_release -cs`/"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo apt-get update
 sudo apt-get install r-base-core r-base-dev
+
+# move .Rprofile to user's home
+mv config/.Rprofile ~/
 
 # 批量安装R包
 # 下面三种都可以执行R文件, 但有细微差别, 
